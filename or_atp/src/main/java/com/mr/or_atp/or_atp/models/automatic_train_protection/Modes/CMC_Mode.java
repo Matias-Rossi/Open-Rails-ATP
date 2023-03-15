0a92a18dc646e5d90ebdc92f7ac89c2114dce1e9
Matias Rossi <matiastadeorossi@gmail.com>
2023-03-15 00:52:34 -0300

    Minor fixes

--- or_atp/src/main/java/com/mr/or_atp/or_atp/models/automatic_train_protection/Modes/CMC_Mode.java
+++ or_atp/src/main/java/com/mr/or_atp/or_atp/models/automatic_train_protection/Modes/CMC_Mode.java
@@ -16,6 +16,9 @@ public class CMC_Mode implements ATP_ModeInterface {
     @Getter private int currentRouteSpeedLimit;
     private double distanceToObjective;
     @Getter private ATP_Panel panelStatus;
+    private double lastSpeed;
+    private double acceleration;
+    private boolean isBrakeApplied;
 
     private int timeOverLimit; //TODO: Pending implementation
 
@@ -29,6 +32,7 @@ public class CMC_Mode implements ATP_ModeInterface {
         currentRouteSpeedLimit = 0;
         distanceToObjective = 0;
         timeOverLimit = 0;
+        lastSpeed = 0;
         panelStatus = ATP_Panel.getInstance();
         panelStatus.reset();
         panelStatus.setCmc_light(LightIndicatorStatus.ON);
@@ -46,21 +50,25 @@ public class CMC_Mode implements ATP_ModeInterface {
             initExecuted = true;
         }
 
+        lastSpeed = currentSpeed;
+        acceleration = (currentSpeed/3.6 - lastSpeed/3.6) / 0.5;
         currentSpeed = simData.getTrainDisplay().getSpeed();
         currentRouteSpeedLimit = (int) Math.round(simData.getTrackMonitor().getCurrentSpeedLimit());
         objectiveSpeed = calculateObjectiveSpeed(simData);
         allowedSpeed = calculateAllowedSpeed();
+        System.out.println("Allowed speed: " + allowedSpeed);
+        isBrakeApplied = simData.getTrainDisplay().isBrakeApplied();
 
         //Update panel
         panelStatus.setSpeeds(currentSpeed, objectiveSpeed, allowedSpeed);
         panelStatus.setTraction_light(isTractionAllowed()? LightIndicatorStatus.ON : LightIndicatorStatus.OFF);
-        panelStatus.setBrake_light(simData.getTrainDisplay().isBrakeApplied()? LightIndicatorStatus.ON : LightIndicatorStatus.OFF);
+        panelStatus.setBrake_light(isBrakeApplied? LightIndicatorStatus.ON : LightIndicatorStatus.OFF);
         panelStatus.setPenalty_light(isEmergencyBrakeForced()? LightIndicatorStatus.ON : LightIndicatorStatus.OFF);
     }
 
     @Override
     public boolean isTractionAllowed() {
-        return currentSpeed < allowedSpeed;
+        return currentSpeed < allowedSpeed && !isBrakeApplied;
     }
 
     @Override
@@ -81,13 +89,21 @@ public class CMC_Mode implements ATP_ModeInterface {
         //Compare distances
         if(closestSignal != null && closestSpeedLimit != null) {
             if(closestSignal.getDistance() < closestSpeedLimit.getDistance()) {
-                setDistanceToObjective(closestSignal.getDistance());
-                int lower = Math.min(closestSignal.getSpeedLimit(), closestSpeedLimit.getSpeedLimit());
-                return Math.min(lower, currentRouteSpeedLimit);
+                if (closestSignal.getSpeedLimit() < closestSpeedLimit.getSpeedLimit()) {
+                    setDistanceToObjective(closestSignal.getDistance());
+                    return Math.min(closestSignal.getSpeedLimit(), currentRouteSpeedLimit);
+                } else {
+                    setDistanceToObjective(closestSpeedLimit.getDistance());
+                    return Math.min(closestSpeedLimit.getSpeedLimit(), currentRouteSpeedLimit);
+                }
             } else {
-                setDistanceToObjective(closestSpeedLimit.getDistance());
-                int lower = Math.min(closestSignal.getSpeedLimit(), closestSpeedLimit.getSpeedLimit());
-                return Math.min(lower, currentRouteSpeedLimit);
+                if (closestSignal.getSpeedLimit() < closestSpeedLimit.getSpeedLimit()) {
+                    setDistanceToObjective(closestSignal.getDistance());
+                    return Math.min(closestSignal.getSpeedLimit(), currentRouteSpeedLimit);
+                } else {
+                    setDistanceToObjective(closestSpeedLimit.getDistance());
+                    return Math.min(closestSpeedLimit.getSpeedLimit(), currentRouteSpeedLimit);
+                }
             }
         } else if(closestSignal != null) {
             setDistanceToObjective(closestSignal.getDistance());
@@ -106,8 +122,19 @@ public class CMC_Mode implements ATP_ModeInterface {
 
     public int calculateAllowedSpeed() {
         // d * 3.6 / react_time = speed
-        double brakingCurve = Math.min(distanceToObjective * 3.6 / 2 + objectiveSpeed, currentRouteSpeedLimit);
-        return (int) Math.round(Math.max(brakingCurve, 10));
+        //double brakingCurve = Math.min(distanceToObjective * 0.25 / 2 + objectiveSpeed, currentRouteSpeedLimit);
+        /*
+         * d = (v^2 - v_r^2) / (2 * a)
+         * Where:
+         * d is the distance required to come to a complete stop from the current speed (in meters).
+         * v is the current speed of the train (in meters per second).
+         * v_r is the speed restriction (in meters per second).
+         * a is the current deceleration of the train (in meters per second squared).
+         */
+
+        double brakingCurve = Math.sqrt(0.75 * Math.max(distanceToObjective - 50, 0) * 2 + Math.pow(objectiveSpeed/3.6, 2)) * 3.6;
+        double minSpeed = Math.min(brakingCurve, currentRouteSpeedLimit);
+        return (int) Math.round(Math.max(minSpeed, 5));
     }
 
     @Override
